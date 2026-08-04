9fa5c5c78da31c70a1dd98d3b3ac311558cfccfe
sivabhaskar6 <gonthinasivabhaskar6@gmail.com>
2026-08-05 00:40:51 +0530

    we make the videos clickale and play that videos my fetching the video by useSearchParams

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,10 +1,15 @@
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 
 const WatchPage = () =>{
 
+    const [searchParams] = useSearchParams();
+
+    const videoId = searchParams.get("v");
+    console.log(videoId);
     const dispatch = useDispatch();
     useEffect(()=>{
         dispatch(closeMenu());
@@ -12,8 +17,17 @@ const WatchPage = () =>{
 
 
     return (
-        <div className="flex flex-col">
-            watch page
+        <div className="flex justify-center items-center ">
+           <iframe
+                width="1200"
+                height="600"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            ></iframe>
         </div>
     )
 }
