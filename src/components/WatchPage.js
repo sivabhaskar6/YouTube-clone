8753f7289036de87afe965b854bfcff9c1d7522c
sivabhaskar6 <gonthinasivabhaskar6@gmail.com>
2026-08-06 20:51:23 +0530

    when we click on videos it will play the video we get the id from the Searchparams

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -29,6 +29,7 @@ const WatchPage = () =>{
                 allowFullScreen
             ></iframe>
         </div>
+        
     )
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,7 @@
 const GOOGLE_API_KEY = `AIzaSyCI7JPQlrnjGkZzEs2AKThQHDluPoneHLc`;
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
+
+
+// https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=AIzaSyCI7JPQlrnjGkZzEs2AKThQHDluPoneHLc
+
