47831ce364cc9de5b55a8f7b9de927f0d5cf168c
sivabhaskar6 <gonthinasivabhaskar6@gmail.com>
2026-09-03 08:28:54 +0530

    Add YouTube API integration to environment varialbe

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
 
 
-const GOOGLE_API_KEY = `AIzaSyCI7JPQlrnjGkZzEs2AKThQHDluPoneHLc`;
 
-export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=49&key=${GOOGLE_API_KEY}`;
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=49&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
 
 // https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=AIzaSyCI7JPQlrnjGkZzEs2AKThQHDluPoneHLc
