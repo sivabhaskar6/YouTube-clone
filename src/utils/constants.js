d198d4ac6b9f259b38f28c83824ad6db95806f3e
sivabhaskar6 <gonthinasivabhaskar6@gmail.com>
2026-09-08 10:15:09 +0530

    removed the explosure of key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=49&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
 
-// https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=AIzaSyCI7JPQlrnjGkZzEs2AKThQHDluPoneHLc
+
 
 export const YOUTUBE_SEARCH_API =
   `/api/suggestions?client=firefox&ds=yt&q=`;
